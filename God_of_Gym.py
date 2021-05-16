6b73f27ef51383f7866002821c14dcc37c537f11
Aptyp-Pirozhkov <84130464+Aptyp-Pirozhkov@users.noreply.github.com>
2021-05-16 12:11:32 +0500

    Refactoring, adding random facts

--- God_of_Gym.py
+++ God_of_Gym.py
@@ -4,6 +4,7 @@ import asyncio
 import time
 import dis_token
 import re
+import random
 import gym_db
 
 bot = commands.Bot(command_prefix='-')
@@ -21,29 +22,47 @@ async def on_ready():
 async def on_message(message):
     if message.author.id != 842356746604904448:  # Check if it's bot's message
         if re.search('г[ао]*ндо*н|г[ао]*рдо*н', message.content, flags=re.I):
-            if gym_db.get_condoms(message.author.id) is None:
-                gym_db.add_member(message.author.id)
+            if gym_db.Condoms().get_condoms(message.author.id) is None:  # if this is the first condom word
+                gym_db.Condoms().add_member(message.author.id)
                 await message.channel.send('`Поздравляю с первым словом *гандон*, теперь ты ♂FUCKING SLAVE♂`')
             else:
-                gym_db.add_condom(message.author.id)
-                await message.channel.send('Кто сказал?')
+                gym_db.Condoms().add_condom(message.author.id)
+                fact_id = random.randint(0, gym_db.Facts().facts_max_id())
+
+                await message.channel.send('```Интересный факт о презервативах:\n' + gym_db.Facts().r_fact(fact_id) + '```')
 
         await bot.process_commands(message)
 
 
 @bot.command(pass_context=True)
 async def condom_ranking(ctx):
-    all_condoms = gym_db.get_all_condoms()
+    all_condoms = gym_db.Condoms().get_all_condoms()
     all_condoms.sort()
-    print(all_condoms)
+    # print(all_condoms)
     ranking_board = '```\tТаблица лидеров по использованным гандонам:\n---------------\n'
     for tuple in all_condoms:
-        member = await ctx.guild.fetch_member(tuple[0])
-        name = member.name
-        ranking_board += f'{name} - {tuple[1]} гандонов'
+        try:
+            member = await ctx.guild.fetch_member(tuple[0])
+            name = member.name
+            ranking_board += f'{name} - {tuple[1]} гандонов\n'
+        except discord.errors.NotFound:
+            pass
+
     ranking_board += '```'
         # ranking_board +=
     await ctx.send(ranking_board)
 
 
+# def add_fact(fact):
+#     facts = gym_db.Facts()
+#     facts.w_fact(fact)
+#
+#
+# def delete_fact():
+#     gym_db.Facts().del_fact(2)
+#
+
+# add_fact("""""")
+# delete_fact()
+# print(gym_db.Facts().facts_max_id())
 bot.run(TOKEN)
--- gym_db.py
+++ gym_db.py
@@ -8,7 +8,8 @@ def init():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS condom_facts (
         id INTEGER,
-        fact TEXT
+        fact TEXT,
+        UNIQUE(fact)
     )
     """)
 
@@ -19,68 +20,98 @@ def init():
     )
     """)
 
-
-def w_fact(id, fact):
-    cursor.execute(f"""
-    INSERT INTO condom_facts VALUES
-    ({id}, '{fact}')
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS ranks (
+        rank TEXT,
+        condoms INTEGER
+    )
     """)
-    connection.commit()
-    return True
 
 
-def r_fact(id):
-    cursor.execute(f"""
-    SELECT fact FROM condom_facts WHERE id = {id} 
-    """)
-    fact = cursor.fetchone()
-    return fact
+class Facts:
+    def __init__(self):
+        pass
 
+    def facts_max_id(self):
+        cursor.execute("""
+                SELECT MAX(id) FROM condom_facts
+                """)
+        num = cursor.fetchall()[0][0]
+        if num is None:
+            num = 0
+        return num
 
-def del_fact(id):
-    cursor.execute(f"""
-    DELETE FROM condom_facts WHERE id = {id}
-    """)
-    return True
-
-
-def add_member(member_id):
-    cursor.execute(f"""
-    INSERT INTO condom_count VALUES
-    ({member_id}, 1)
-    """)
-    connection.commit()
-    return True
-
-
-def add_condom(member_id):
-    cursor.execute(f"""
-    SELECT count FROM condom_count WHERE member_id={member_id}
-    """)
-    c = cursor.fetchone()[0]
-    cursor.execute(f"""
-        UPDATE condom_count SET count = {c + 1} WHERE member_id = {member_id}
+    def w_fact(self, fact):
+        cursor.execute(f"""
+        INSERT OR IGNORE INTO condom_facts VALUES
+        ({Facts.facts_max_id(self) + 1}, "{fact}")
         """)
-    connection.commit()
-    return True
+        connection.commit()
+        return True
+
+    def r_fact(self, id):
+        cursor.execute(f"""
+        SELECT fact FROM condom_facts WHERE id = {id} 
+        """)
+        fact = cursor.fetchone()[0]
+        return fact
+
+    def del_fact(self, id):
+        cursor.execute(f"""
+        DELETE FROM condom_facts WHERE id = {id}
+        """)
+        connection.commit()
+        return True
+
+    def all_facts(self):
+        cursor.execute("""
+        SELECT * FROM condom_facts
+        """)
+        return cursor.fetchall()
 
 
-def get_condoms(member_id):
-    cursor.execute(f"""
+class Condoms:
+    def __init__(self):
+        pass
+
+    def add_member(self, member_id):
+        cursor.execute(f"""
+        INSERT INTO condom_count VALUES
+        ({member_id}, 1)
+        """)
+        connection.commit()
+        return True
+
+    def add_condom(self, member_id):
+        cursor.execute(f"""
         SELECT count FROM condom_count WHERE member_id={member_id}
         """)
-    count = cursor.fetchone()
-    if count is not None:
-        count = count[0]
-    return count
+        c = cursor.fetchone()[0]
+        cursor.execute(f"""
+            UPDATE condom_count SET count = {c + 1} WHERE member_id = {member_id}
+            """)
+        connection.commit()
+        return True
+
+    def get_condoms(self, member_id):
+        cursor.execute(f"""
+            SELECT count FROM condom_count WHERE member_id={member_id}
+            """)
+        count = cursor.fetchone()
+        if count is not None:
+            count = count[0]
+        return count
+
+    def get_all_condoms(self):
+        cursor.execute(f"""
+            SELECT * FROM condom_count
+            """)
+        count = cursor.fetchall()
+        return count
 
 
-def get_all_condoms():
-    cursor.execute(f"""
-        SELECT member_id,count FROM condom_count
-        """)
-    count = cursor.fetchall()
-    return count
+
+
 
 
 
